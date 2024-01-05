b31ecb5187651a5415fd21a7d8d0eacaedc2c26f
Louis Mollick <louismollick@gmail.com>
2024-01-04 23:17:59 -0500

    Updating stack with Request Authorizer keeps type TOKEN

--- lib/constructs/my-authorizer.ts
+++ lib/constructs/my-authorizer.ts
@@ -1,10 +1,12 @@
 import * as cdk from 'aws-cdk-lib';
+import { IdentitySource } from 'aws-cdk-lib/aws-apigateway';
 import { Construct } from 'constructs';
 
-export default class MyAuthorizer extends cdk.aws_apigateway.TokenAuthorizer {
+export default class MyAuthorizer extends cdk.aws_apigateway.RequestAuthorizer {
   constructor(scope: Construct, handler: cdk.aws_lambda.IFunction) {
     super(scope, 'MyAuthorizer', {
       handler,
+      identitySources: [IdentitySource.header('Authorization')],
     });
   }
 }
